9c3f4c6de47ce95489fada09152f1371de06244e
DEFAULT3\colaman <colaman@nate.com>
2024-02-25 19:16:08 +0900

    5장 이벤트모델 준비

---
+++ java_network_girl_netty_0007/src/main/java/com/colamanlabs/netty/networkgirl/s0007/JavaNetworkGirlNetty0007Application.java
@@ -0,0 +1,27 @@
+package com.colamanlabs.netty.networkgirl.s0007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class JavaNetworkGirlNetty0007Application
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaNetworkGirlNetty0007Application.class, args);
+    }
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() throws Exception
+    {
+        log.info(String.format("[JavaNetworkGirlNetty0007Application/init] BEGIN"));
+        log.info(String.format("[JavaNetworkGirlNetty0007Application/init] END"));
+    }
+    
+}
---
+++ java_network_girl_netty_0007/src/test/java/com/colamanlabs/netty/networkgirl/s0007/JavaNetworkGirlNetty0007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.netty.networkgirl.s0007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNetworkGirlNetty0007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
